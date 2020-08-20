3eaa57abd88f724f277786b4f2971ea303088758
nivsmall <54941614+nivsmall@users.noreply.github.com>
2020-08-20 16:56:04 +0300

    commit

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ print('hello world')
 
 #labels_dict = data_processing.load_labels('bdd100k/used_labels/train_aug_labels.json')
 #examples_cnt = data_processing.write_tfrecord('bdd100k/tfrecords/train_aug.tfrecords', labels_dict,
-                                              'bdd100k/used_images/train/')
+#                                              'bdd100k/used_images/train/')
 #print(examples_cnt)
 
 #input_pipeline.read_tfrecord('bdd100k/tfrecords/train_test.tfrecords', show=True)
