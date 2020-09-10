847e6a970ea5bb433e1da311b584099e9e7fd852
nivsmall <54941614+nivsmall@users.noreply.github.com>
2020-09-10 12:46:35 +0300

    BatchNorm layers added

--- alexnet_model_seq.py
+++ alexnet_model_seq.py
@@ -5,33 +5,49 @@ from tensorflow.keras import layers, models
 
 
 def create_alexnet(input_shape, num_classes, drop_out_rate=0):
-    ''''
+    """
     initializers: he_normal (kaiming) -- mean: 0 -- std: sqrt(2/fan-in)
-    '''
+    """
     model = models.Sequential()
 
     model.add(layers.Conv2D(96, kernel_size=(11, 11), strides=4,
-                            padding='valid', activation='relu',
+                            padding='valid', activation=None,
                             input_shape=input_shape, kernel_initializer='he_normal'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.ReLU())
     model.add(layers.MaxPool2D(pool_size=(3, 3), strides=1, padding='valid'))
+    model.add(layers.Dropout(drop_out_rate))
 
     model.add(layers.Conv2D(256, kernel_size=(5, 5), strides=1,
-                            padding='same', activation='relu',
+                            padding='same', activation=None,
                             kernel_initializer='he_normal'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.ReLU())
     model.add(layers.MaxPool2D(pool_size=(3, 3), strides=(2, 2), padding='same'))
+    model.add(layers.Dropout(drop_out_rate))
 
     model.add(layers.Conv2D(384, kernel_size=(3, 3), strides=1,
-                            padding='same', activation='relu',
+                            padding='same', activation=None,
                             kernel_initializer='he_normal'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.ReLU())
+    model.add(layers.Dropout(drop_out_rate))
 
     model.add(layers.Conv2D(384, kernel_size=(3, 3), strides=1,
-                            padding='same', activation='relu',
+                            padding='same', activation=None,
                             kernel_initializer='he_normal'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.ReLU())
+
+    model.add(layers.Dropout(drop_out_rate))
 
     model.add(layers.Conv2D(384, kernel_size=(3, 3), strides=1,
-                            padding='same', activation='relu',
+                            padding='same', activation=None,
                             kernel_initializer='he_normal'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.ReLU())
     model.add(layers.MaxPool2D(pool_size=(3, 3), strides=(2, 2), padding='valid'))
+    model.add(layers.Dropout(drop_out_rate))
 
     model.add(layers.Flatten())
 
@@ -42,10 +58,14 @@ def create_alexnet(input_shape, num_classes, drop_out_rate=0):
 
     #   If GPU doesn't have enough RAM use smaller Dense layers (e.i. 256 each):
     #   Possibly add dropout:
-    # model.add(layers.Dropout(drop_out_rate))
-    model.add(layers.Dense(256, activation='relu', kernel_initializer='he_normal'))
-    model.add(layers.Dense(256, activation='relu', kernel_initializer='he_normal'))
-    model.add(layers.Dense(256, activation='relu', kernel_initializer='he_normal'))
+    model.add(layers.Dense(512, activation=None, kernel_initializer='he_normal'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.ReLU())
+    model.add(layers.Dropout(drop_out_rate))
+
+    model.add(layers.Dense(128, activation=None, kernel_initializer='he_normal'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.ReLU())
 
     model.add(layers.Dense(num_classes, activation='sigmoid'))
     model.summary()
