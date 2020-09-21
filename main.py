b986278d171a736ef3d1da3261ae9cb5c3d966d9
nivsmall <54941614+nivsmall@users.noreply.github.com>
2020-09-21 15:36:16 +0300

    commit

--- main.py
+++ main.py
@@ -2,22 +2,39 @@ import FilterData
 import Labelling
 import data_processing
 import input_pipeline
-import train
+from train import train
 print('hello world')
 
 
-#data = FilterData.filterData(['clear', 'overcast', 'undefined', 'partly', 'cloudy'], ['tunnel', 'highway'],
-#                             ['daytime', 'dawn/dusk', 'undefined'], 'bdd100k/labels/bdd100k_labels_images_test.json')
-#FilterData.preview_filtered_data(data, 'bdd100k/bdd100k/images/100k/test')
-#FilterData.relocate_wanted_images(data,'bdd100k/bdd100k/images/100k/test', 'bdd100k/used_images/test')
+# Filter images to keep wanted attributes only:
+data = FilterData.filterData(['clear', 'overcast', 'undefined', 'partly', 'cloudy'], ['tunnel', 'highway', 'residential', 'undefined', 'city street'],
+                             ['daytime', 'dawn/dusk'], 'bdd100k/labels/bdd100k_labels_images_val.json')
+FilterData.preview_filtered_data(data, 'bdd100k/used_images/val/')
 
-#Labelling.ManualLabelling(DATA_DIR="bdd100k/used_images/train/")
-#Labelling.labels_Json_from_image_paths("bdd100k/used_images/train/", 'bdd100k/used_labels/train_aug_labels.json')
+# Relocate filtered images:
+FilterData.relocate_wanted_images(data, 'bdd100k/bdd100k/images/100k/val/', 'bdd100k/used_images/val/')
 
-#labels_dict = data_processing.load_labels('bdd100k/used_labels/train_aug_labels.json')
-#examples_cnt = data_processing.write_tfrecord('bdd100k/tfrecords/train_aug.tfrecords', labels_dict,
-#                                              'bdd100k/used_images/train/')
-#print(examples_cnt)
+# Manually label data: (if needed)
+Labelling.ManualLabelling(DATA_DIR="bdd100k/used_images/val/")
 
-#input_pipeline.read_tfrecord('bdd100k/tfrecords/train_test.tfrecords', show=True)
+# Separate data by class into folders:
+Labelling.move_images_to_class_folders("bdd100k/used_images/val/")
+
+# Create Json file to hold labels from split folders
+Labelling.labels_Json_from_image_paths("bdd100k/used_images/val/", 'bdd100k/used_labels/val.json')
+
+# Write a tf.record file using image-label pairs from Json file
+labels_dict = data_processing.load_labels('bdd100k/used_labels/val.json')
+examples_cnt = data_processing.write_tfrecord('bdd100k/tfrecords/val.tfrecords', labels_dict,
+                                              'bdd100k/used_images/val/', False)
+print(examples_cnt)
+
+# Verify data before training:
+input_pipeline.read_tfrecord('bdd100k/tfrecords/train.tfrecords', show=True)
+
+# augment data:
+data_processing.balance_data_via_augmentation('bdd100k/used_images/train/', False)
+
+train('bdd100k/tfrecords/train.tfrecords', transfer=False)
+
 
--- train.py
+++ train.py
@@ -9,12 +9,12 @@ from alexnet_model_seq import create_alexnet
 from data_processing import load_labels
 import input_pipeline
 
-EPOCHS = 3
+EPOCHS = 40
 INPUT_SHAPE = (227, 227, 3)
 NUM_CLASSES = 1
 BATCH_SIZE = 16
 lr = 1e-6
-drop_out = 0.5
+drop_out = 0
 batchNorm = False
 
 
@@ -36,7 +36,7 @@ def train(tfrecord_path, transfer=False):
     train_ds = input_pipeline.read_tfrecord(tfrecord_path, show=False)
     train_ds = train_ds.shuffle(buffer_size=16384).batch(BATCH_SIZE)
 
-    val_ds = input_pipeline.read_tfrecord('bdd100k/tfrecords/val_highway.tfrecords', show=False)
+    val_ds = input_pipeline.read_tfrecord('bdd100k/tfrecords/val.tfrecords', show=False)
     val_ds = val_ds.shuffle(buffer_size=2048).batch(BATCH_SIZE)
 
     model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=lr, amsgrad=True),
@@ -58,7 +58,7 @@ def train(tfrecord_path, transfer=False):
     print(history)
 
     val_loss, val_acc = model.evaluate(val_ds, verbose=2)
-    plot_training_session(history, lr, drop_out, batchNorm, val_loss, val_acc, )
+    plot_training_session(history, lr, drop_out, batchNorm, val_loss, val_acc, start_time)
 
     return
 
@@ -69,7 +69,7 @@ def plot_training_session(history, l_r, d_o, b_n, val_loss, val_acc, start_time)
     else: b_n = ''
 
     fig, (ax1, ax2) = plt.subplots(2)
-    fig.suptitle('Training Session: {0}\n Val loss-acc: {1}-{2}'.format(start_time, val_loss, val_acc))
+    fig.suptitle('Training Session: {0}\n Val: loss-acc = {1}-{2}'.format(start_time, val_loss, val_acc))
     ax1.plot(history.history['accuracy'], label='accuracy')
     ax1.plot(history.history['val_accuracy'], label='val_accuracy')
     ax1.set(xlabel='Epoch', ylabel='Accuracy')
